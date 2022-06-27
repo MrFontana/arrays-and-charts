fd24ec832879c01d8f9c12c7d4b708b1d98a8bd4
kfontana@crescendocollective.com <kfontana@crescendocollective.com>
2022-06-27 11:34:48 -0700

    Finished module about sets and weak sets

--- javascript-arrays-collections/03/demos/Module_3_Project_Files/01 Starter Project/js/page.js
+++ javascript-arrays-collections/03/demos/Module_3_Project_Files/01 Starter Project/js/page.js
@@ -4,55 +4,117 @@ let pieCtx = document.getElementById('deptSales').getContext('2d');
 
 let yearlyLabel = document.getElementById('yearlyTotal');
 
-// Monthly Totals
+// Values from the form
+let newAmount = document.getElementById('itemAmount');
+let newMonth = document.getElementById('monthId');
+
+let hikingRadio = document.getElementById('hiking');
+let runningRadio = document.getElementById('running');
+let huntingRadio = document.getElementById('hunting');
+
 let yearlyTotal = 0;
 
-// Code goes here
+const monthlySales = new Set();
+const monthlyLabels = new Set();
+
+const categories = new WeakSet();
+
+let hiking = {category: 'Hiking'};
+let running = {category: 'Running'};
+let hunting = {category: 'Hunting'};
+
+function addSale() {
+	monthlySales.add(parseInt(newAmount.value));
+	monthlyLabels.add(newMonth.value);
+
+	yearlyTotal = 0;
+
+	monthlySalesChart.data.datasets.forEach((dataset) => {
+		dataset.data = [];
+	})
+
+	for (let amount of monthlySales) {
+		yearlyTotal += amount;
+		yearlyLabel.innerHTML = yearlyTotal;
+
+		monthlySalesChart.data.datasets.forEach((dataset) => {
+			dataset.data.push(amount);
+		})
+
+	}
+
+	monthlySalesChart.data.labels = Array.from(monthlyLabels);
+
+	monthlySalesChart.update();
+
+	if (hikingRadio.checked) {
+		categories.add(hiking);
+	} else if (runningRadio.checked) {
+		categories.add(running);
+	} else if (huntingRadio.checked) {
+		categories.add(hunting);
+	} else {
+		// do something else
+	};
+
+	console.log(categories);
+	 
+};
+
+function deleteVal() {
+	monthlySales.forEach((value1, value2, monthlySales) => {
+		alert(value1);
+	});
+};
+
+function addTotal() {
+	
+}
 
 // Bar chart
 var monthlySalesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: monthlyLabels,
-        datasets: [{
-            label: '# of Sales',
-            data: monthlySales,
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+	type: 'bar',
+	data: {
+		labels: [],
+		datasets: [{
+			label: '# of Sales',
+			data: [],
+			backgroundColor: [
+				'rgba(238, 184, 104, 1)',
+				'rgba(75, 166, 223, 1)',
+				'rgba(239, 118, 122, 1)',
+			],
+			borderWidth: 0
+		}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		}
+	}
 });
 
 // Pie Chart
 var deptSalesChart = new Chart(pieCtx, {
-    type: 'pie',
-    data: {
-        labels: deptLabels,
-        datasets: [{
-            label: '# of Sales',
-            data: deptSales,
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        
-    }
+	type: 'pie',
+	data: {
+		labels: deptLabels,
+		datasets: [{
+			label: '# of Sales',
+			data: deptSales,
+			backgroundColor: [
+				'rgba(238, 184, 104, 1)',
+				'rgba(75, 166, 223, 1)',
+				'rgba(239, 118, 122, 1)',
+			],
+			borderWidth: 0
+		}]
+	},
+	options: {
+		
+	}
 })
